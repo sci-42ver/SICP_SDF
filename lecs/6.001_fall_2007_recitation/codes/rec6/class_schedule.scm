380b9353c6019f271c6f2cb9607c15e868c83c6c
sci-42ver <wujsCM@tuta.io>
2024-07-15 13:41:41 +0800

    update rec06

--- lecs/6.001_fall_2007_recitation/codes/rec6/class_schedule.scm
+++ lecs/6.001_fall_2007_recitation/codes/rec6/class_schedule.scm
@@ -33,19 +33,26 @@
   (sum_lst (map get-class-total-units sched)))
 
 ;; 4 Assume `append` is O(1). Then helper/drop-class is time O(n) and space O(1).
+
+;; See sol. `((null? sched) nil)` to consider the case where `classnum` is not in `sched`.
+;; Here I use iter. So space O(1).
 (define (drop-class sched classnum)
   (define (helper prev rest)
     (let ((cur (car sched)))
       (if (= (get-class-number cur) classnum)
-          (append prev (cdr rest))
-          (helper (append prev (list cur)) (cdr rest)))))
+        (append prev (cdr rest))
+        (helper (append prev (list cur)) (cdr rest)))))
   (helper (empty-schedule) sched))
 
 ;; 5 time O(n^2) space O(1)
+
+;; Sol uses \Theta based on the worst case.
+;; `(total-scheduled-units sched)` has space \Theta(n).
 (define (credit-limit sched max-credits)
   (if (>= (total-scheduled-units sched) max-credits)
-      (credit-limit (drop-class sched (get-class-number (car sched))) max-credits)
-      sched))
+    ;; See sol, here cdr is faster.
+    (credit-limit (drop-class sched (get-class-number (car sched))) max-credits)
+    sched))
 
 (define test_units (make-units 1 2 3))
 (define test_units (make-units 2 3 4))
@@ -54,3 +61,47 @@
 (total-scheduled-units test_schedule)
 (drop-class test_schedule 100)
 (credit-limit test_schedule 10)
+
+;;; HOP
+(define (make-student number sched-checker)
+  (list number (list) sched-checker))
+(define get-student-number car)
+(define get-student-schedule cadr)
+(define get-student-checker caddr)
+(define (update-student-schedule student schedule)
+  (if ((get-student-checker student) schedule)
+    (list (get-student-number student)
+          schedule
+          (get-student-checker student))
+    (error "invalid schedule")))
+
+;; 6
+(make-student 575904467 (lambda (sched) (not (null? sched))))
+
+;; 7
+(make-student 575904467 (lambda (sched) (<= (total-scheduled-units sched) 54)))
+
+;; 8
+(define (class-numbers schedule)
+  (map get-class-number schedule))
+
+;; 9
+(define (drop-class sched classnum)
+  (filter (lambda (class) (not (= (get-class-number class) classnum))) sched))
+
+;; 10
+;; TODO `total-scheduled-units` must iterate all so at least \Theta(n).
+;; Then check all classes -> \Theta(n^2).
+
+;; This is based on addition, so \Theta(n).
+(define (credit-limit sched limit)
+  (define (helper sched)
+    (if (null? sched)
+        (list (list) 0)
+        (let ((res (helper (cdr sched))) ; This will be called recursively first.
+              (newunits (get-class-total-units (car sched))))
+          (if (< (+ newunits (cadr res)) limit)
+            (list (add-class (car sched) (car res)) ; \Theta(1)
+                  (+ newunits (cadr res)))
+            res))))
+  (car (helper sched)))
