df975dbdce35eb78df24699a17cc8c0b5289e876
sci-42ver <wujsCM@tuta.io>
2024-08-05 10:24:25 +0800

    finish ps01 2.a

--- 6.945_ps_self_try/ps01/code/2_a.py
+++ 6.945_ps_self_try/ps01/code/2_a.py
@@ -1,3 +1,16 @@
+"""
+2.1 has 
+compose
+parallel-combine
+spread-combine
+discard-argument
+curry-argument
+permute-arguments
+
+I choose the first 3 to implement.
+
+Here I didn't implement `restrict-arity` but just ensure that we receive the correct number of args.
+"""
 # https://www.geeksforgeeks.org/how-to-pass-multiple-arguments-to-function/
 import inspect
 
@@ -34,13 +47,13 @@ def compose(f, g):
       # else:
       #   return f(*[g_result])
 
-      print("g result:", (g(*arguments),), *(g(*arguments),))
+      # print("g result:", (g(*arguments),), *(g(*arguments),))
       if type(g(*arguments)) is tuple:
         # g_result=*g(*arguments) # not allowed by syntax ... I won't dig into python syntax.
         # if len(g_result)!=f_arity:
         #   print("f receives the wrong Arg number")
         # else:
-        print("g tuple result", *g(*arguments), g(*arguments))
+        # print("g tuple result", *g(*arguments), g(*arguments))
         return f(*g(*arguments))
       else:
         return f(*(g(*arguments),))
@@ -51,7 +64,9 @@ def compose(f, g):
 
 assert compose(lambda x: x+2, lambda x: x*x)(2) == 2*2+2
 # this test is expected to throw error.
-print("compose test_2", compose(lambda x: x+2, lambda x: x*x)(2, 2))
+print("compose test_2 began")
+print(compose(lambda x: x+2, lambda x: x*x)(2, 2))
+print("compose test_2 finished")
 # compose(lambda x:x, lambda x,y:(x,y))(2,3)==(2,3) # should throw error due to f arity is wrong.
 assert compose(lambda x, y: x+y, lambda x, y: (x, y))(2, 3) == 5
 
@@ -78,6 +93,7 @@ def parallel_apply(f, g):
         f_result = (f_result,)
       if type(g_result) is not tuple:
         g_result = (g_result,)
+      # correspond to `append` in "function-combinators.scm". `values` is implied in tuple.
       return f_result+g_result
     except:
       pass
@@ -93,12 +109,52 @@ def parallel_combine(h, f, g):
 # print(parallel_apply(lambda x:x+2, lambda x:x*x)(2))
 assert parallel_combine(lambda x, y: x*y, lambda x: x +
                         2, lambda x: x*x)(2) == 16
-print("test_4", parallel_combine(list, lambda x: x+2, lambda x, y: x*x)(2))
-print("test_5", parallel_combine(list, lambda x: x+2, lambda x: x*x)(2, 2))
+print("error test began")
+print("test_4")
+print(parallel_combine(list, lambda x: x+2, lambda x, y: x*x)(2))
+print("test_5")
+print(parallel_combine(list, lambda x: x+2, lambda x: x*x)(2, 2))
+print("error test finished")
 
-# test from text-examples.scm
-# list doesn't accpet multiple arguments.
+"""
+1. test from text-examples.scm
+2. list doesn't accpet multiple arguments.
+3. Here "compose Arg number error" is due to we don't set arity of "the_composition".
+"""
 assert parallel_combine(lambda *arguments: list(arguments), lambda x, y, z: (
-    x, y, z), lambda x, y, z: (z, y, x))("a", "b", "c") == ['a', 'b', 'c', 'c', 'b', 'a']
+    x, y, z), lambda x, y, z: (z, y, x))('a', 'b', 'c') == ['a', 'b', 'c', 'c', 'b', 'a']
 assert parallel_combine(lambda *arguments: list(arguments), lambda x, y, z: [
-                        "foo", x, y, z], lambda x, y, z: ["bar", x, y, z])("a", "b", "c") == [["foo", 'a', 'b', 'c'], ["bar", 'a', 'b', 'c']]
+                        "foo", x, y, z], lambda x, y, z: ["bar", x, y, z])('a', 'b', 'c') == [["foo", 'a', 'b', 'c'], ["bar", 'a', 'b', 'c']]
+
+
+def spread_apply(f, g):
+  g_arity = len(inspect.signature(g).parameters)
+  f_arity = len(inspect.signature(f).parameters)
+
+  def the_composition(*arguments):
+    try:
+      if len(arguments) != g_arity+f_arity:
+        print("spread_apply the_composition Arg number error")
+      f_result = f(*(arguments[0:f_arity]))
+      g_result = g(*(arguments[f_arity:]))
+      if type(f_result) is not tuple:
+        f_result = (f_result,)
+      if type(g_result) is not tuple:
+        g_result = (g_result,)
+      # correspond to `append` in "function-combinators.scm". `values` is implied in tuple.
+      return f_result+g_result
+    except:
+      pass
+  return the_composition
+
+
+def spread_combine(h, f, g):
+  # https://stackoverflow.com/a/691274/21294350 * here is like values in Scheme.
+  # Here we need to "return". notice the syntax difference from Scheme.
+  return compose(h, spread_apply(f, g))
+
+
+assert spread_combine(lambda *arguments: list(arguments), lambda x, y: (
+    x, y), lambda x, y, z: (z, y, x))('a', 'b', 'c', 'd', 'e') == ['a', 'b', 'e', 'd', 'c']
+assert spread_combine(lambda *arguments: list(arguments), lambda x, y: [
+    "foo", x, y], lambda x, y, z: ["bar", x, y, z])('a', 'b', 'c', 'd', 'e') == [["foo", 'a', 'b'], ["bar", 'c', 'd', 'e']]
