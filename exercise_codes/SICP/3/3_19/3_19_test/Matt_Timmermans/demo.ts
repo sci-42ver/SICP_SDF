ad1390bf8db9d17c3a50fad134ee8aeca55790e7
sci-42ver <wujsCM@tuta.io>
2024-11-06 19:42:16 +0800

    review Matt_Timmermans test and think about the cycle condition for the codes with one brief proof

--- exercise_codes/SICP/3/3_19/3_19_test/Matt_Timmermans/demo.ts
+++ exercise_codes/SICP/3/3_19/3_19_test/Matt_Timmermans/demo.ts
@@ -1,5 +1,6 @@
 /**
  * Compile it: https://code.visualstudio.com/docs/typescript/typescript-tutorial
+ * and run `tsc -p .;node demo.js` https://stackoverflow.com/a/33536053/21294350
  * 
  * The difference from the compiled result demo.js
  * 1. no type related things like 
@@ -10,11 +11,16 @@
  * 2. expand "?."
  */
 /**
- * 0. https://www.typescriptlang.org/docs/handbook/variable-declarations.html#block-scoped-variable-capturing
+ * 0. About let
+ * 0.a. https://www.typescriptlang.org/docs/handbook/variable-declarations.html#block-scoped-variable-capturing
  * getCity = function ()... is similar to env of func in Scheme.
- * 1. > Some readers might do a double-take at this example. The variable x was declared within the if block, and yet we were able to access it from *outside that block*.
+ * 0.b. > Some readers might do a double-take at this example. The variable x was declared within the if block, and yet we were able to access it from *outside that block*.
  * compare it with
  * > // Error: 'b' doesn't exist here
+ * 1. whether pass by reference https://www.damirscorner.com/blog/posts/20231117-PassFieldByReferenceInTypeScript.html
+ * since here class is not used, so assume all are passed by value.
+ * 1.a. => meaning https://stackoverflow.com/a/34274626/21294350
+ * 1.b. Or use object encapsulatation to "pass by reference" https://stackoverflow.com/a/35608020/21294350
  */
 // define type for future checking and usage https://www.typescriptlang.org/docs/handbook/interfaces.html
 interface Node {
@@ -46,9 +52,21 @@ function checkTree(root: Node): boolean {
 
   // LEVEL N
   // We'll set this to a left child if the tree is invalid
-  let invalidNode: Node | undefined;
+  let invalidNode: Node | undefined; // default to be `undefined` which is considered as false when as predicate.
   let next = findLeftLink(levelStart);
   // If no left, then all levels have been visited.
+  /**
+   * if we find one cycle, we will stop at that level until the location where the cycle is detected.
+   * We set 
+   * 0. invalidNode to the start of that right seq.
+   * 1. levelStart to the level where the last concatenation occurs.
+   * the concatenation `levelEnd.right = s;` will stop when s has one cycle.
+   * Then all the right concatenation list will only have self cycles.
+   * So the following codes works fine since we haven't take that detected cycle into what to be recovered.
+   * That means the structure is same as the result for normal tree, each level being one right list with one end cycle.
+   * 1.a. Notice in `while (nextLevel && next) { ...}` we only check with one more left step for each node in `next.left` and `findLeftLink(next.right)`
+   * So it is fine to have one cycle in the right link list of that "left step".
+   */
   while (next) {
     let levelEnd = validateRightList(next.left!);
     if (!levelEnd) {
@@ -158,14 +176,16 @@ function validateRightList(n: Node): Node | undefined {
       return undefined; // found a cycle
     }
   }
-  // add self cycle.
   /**
    * IGNORE:  TODO Why not always set n.right=n when n.right is null.
    * Fixed in OP modification.
    */
+  // forward to the rightmost node if not being there.
   if (n.right) {
     n = n.right;
   }
+  // add self cycle.
+  n.right = n;
   return n;
 }
 
@@ -199,7 +219,12 @@ function findLeftLink(n: Node | undefined): Node | undefined {
  * test
  */
 function treeToString(root: Node) : string {
+  // maybe Record is better https://stackoverflow.com/a/30019750/21294350
+  // https://www.reddit.com/r/typescript/comments/39taz3/comment/cs69xpj/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+  // implies why we need es2015. https://tc39.es/ecma262/multipage/keyed-collections.html https://262.ecma-international.org/6.0/#sec-map-constructor
+  // <> is similar to the following `as` as that link shows "explicitly pass the type in the angle brackets" https://www.typescriptlang.org/docs/handbook/2/generics.html#hello-world-of-generics
   const map = new Map<Node, number>();
+  // https://stackoverflow.com/a/36742170/21294350
   const list: string[] = [];
   function f(n: Node | undefined) {
     if (!n) {
@@ -217,9 +242,12 @@ function treeToString(root: Node) : string {
     list.push(')');
   }
   f(root);
+  // console.log(`test output inside`);
   return list.join('');
 }
 
+// as https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#type-assertions
+// {} https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#interfaces
 const testTree = {
   right: {
     left: {
@@ -242,6 +270,7 @@ console.log(`recovered: ${treeToString(testTree)}`);
 
 testTree.right!.right!.left!.right = testTree.right!.left;
 
+// 2 means n,n.right has been put into map before n.right.left.
 console.log(`test Tree: ${treeToString(testTree)}`);
 console.log(`valid: ${checkTree(testTree)}`);
 console.log(`recovered: ${treeToString(testTree)}`);
