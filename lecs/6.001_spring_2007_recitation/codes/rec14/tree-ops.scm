c9b4e06caf9df047922f85732f9c090a4ced4bdd
sci-42ver <wujsCM@tuta.io>
2024-12-25 19:07:25 +0800

    finish SDF exercise 4.19 before d.

--- lecs/6.001_spring_2007_recitation/codes/rec14/tree-ops.scm
+++ lecs/6.001_spring_2007_recitation/codes/rec14/tree-ops.scm
@@ -1,6 +1,10 @@
 ;; from lec12
 (define (leaf? obj) (not (pair? obj)))
 
+;; similar to https://stackoverflow.com/a/61080168/21294350 
+;; and consider that tree is null in map.
+;; similar to https://stackoverflow.com/a/35301730/21294350
+;; but don't assume car can't be sub-tree.
 (define (tree-map f tree)
   (if (leaf? tree)
     (f tree)
